5b724fe2b9e65016b7babd07cc4ba68f864747d1
dmetrios <diman23420@yandex.ru>
2025-03-11 14:59:35 +0400

    котик-коммитик

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,24 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 //@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(DemoApplication.class, args);
-        String s = ("музаfазум");
-        System.out.println(palindromeStringBuilder(s));
 
-        TreeNode root = new TreeNode(6);
+        TreeNode root = new TreeNode(1);
         TreeNode node1 = new TreeNode(2);
-        TreeNode node2 = new TreeNode(8);
-        TreeNode node3 = new TreeNode(0);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(7);
-        TreeNode node6 = new TreeNode(9);
-        TreeNode node7 = new TreeNode(3);
-        TreeNode node8 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(3);
+        TreeNode node7 = new TreeNode(4);
+        TreeNode node8 = new TreeNode(4);
+        TreeNode node9 = new TreeNode(4);
+        TreeNode node10 = new TreeNode(4);
+        TreeNode node11 = new TreeNode(4);
+        TreeNode node12 = new TreeNode(4);
+        TreeNode node13 = new TreeNode(5);
+        TreeNode node14 = new TreeNode(5);
 
         root.left = node1;
         root.right = node2;
@@ -32,12 +37,19 @@ public class DemoApplication {
         node2.left = node5;
         node2.right = node6;
 
-        node3.left = null;
-        node3.right = null;
+        node3.left = node7;
+        node3.right = node8;
 
-        node4.left = node7;
-        node4.right = node8;
-        System.out.println(lowestCommonAncestor(root, node1, node4).val);
+        node4.left = node9;
+        node4.right = node10;
+
+        node5.left = node11;
+        node5.right = node12;
+
+        node7.left = node13;
+        node7.right = node14;
+
+        System.out.println(isBalanced(root));
     }
 
 
@@ -98,6 +110,42 @@ public class DemoApplication {
         }
 
         return root;
+    }
+
+    private static boolean isBalanced(TreeNode root) {
+        System.out.println(height(root));
+        return height(root) >= 0;
+    }
+
+    private static int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    private static boolean isAnagram(String word1, String word2) {
+        Set<Character> set1 = new HashSet<>();
+
+        for (char c : word1.toCharArray()) {
+            set1.add(c);
+        }
+        int sizeSet1 = set1.size();
+        for (char c : word2.toCharArray()) {
+            set1.add(c);
+        }
+        int sizeSet2 = set1.size();
+
+        if (sizeSet1 != sizeSet2) {
+            return false;
+        }
+        return true;
     }
 }
 
